ab11365359fbe640b7877019735d0e52dd2e4b81
Dmitry Konovalov <dmitry.konovalov@waveaccess.global>
2023-03-07 14:47:00 +0530

    added markdown component added support for external css (for markdown component)

--- src/features/blog/content-edit.tsx
+++ src/features/blog/content-edit.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {ContentDetailsDto} from "../../lib/network/swagger-client";
+import {ContentDetailsDto, ContentUpdateDto} from "../../lib/network/swagger-client";
 import {useRequestContext} from "../../providers/request-provider";
 import {ContentEditContainer} from "./index.styled";
 import {
@@ -9,10 +9,11 @@ import {
   ModuleHeaderSubtitleContainer,
   ModuleHeaderTitleContainer
 } from "../../components/module";
-import {Breadcrumbs, Link, Typography} from "@mui/material";
+import {Breadcrumbs, Card, CardContent, Grid, Link, TextField, Typography} from "@mui/material";
 import {NavigateNext} from "@mui/icons-material";
 import {CoreModule, rootRoute} from "../../lib/router";
 import {GhostLink} from "../../components/ghost-link";
+import MDEditor from '@uiw/react-md-editor';
 
 interface ContentEditProps {
   readonly?: boolean;
@@ -22,6 +23,7 @@ export const ContentEdit = (props: ContentEditProps) => {
   const {client} = useRequestContext();
   const {id} = useParams();
   const [contentItem, setContentItem] = useState<ContentDetailsDto>();
+  const [updatedContentItem, setUpdatedContentItem] = useState<ContentUpdateDto>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
@@ -40,6 +42,19 @@ export const ContentEdit = (props: ContentEditProps) => {
     })();
   }, [client, id])
 
+  const updateValue = (fieldName: keyof ContentUpdateDto, value?: string) => {
+    setUpdatedContentItem((prev) => ({...(prev || contentItem), [fieldName]: value}));
+    console.log(["updateValue", updatedContentItem]);
+  };
+
+  const getValue = (fieldName: keyof ContentUpdateDto): string | boolean | null | undefined => {
+    return updatedContentItem && typeof updatedContentItem[fieldName] !== "undefined"
+      ? updatedContentItem[fieldName]
+      : contentItem
+        ? contentItem[fieldName]
+        : "";
+  }
+
   return (
     <>
       <ModuleHeaderContainer>
@@ -54,15 +69,49 @@ export const ContentEdit = (props: ContentEditProps) => {
             <Link to={`${rootRoute}${CoreModule.blog}`} component={GhostLink} underline="hover">
               Blog
             </Link>
-            <Typography variant="body1">{contentItem && contentItem.title}</Typography>
+            <Typography
+              variant="body1">{(updatedContentItem && updatedContentItem.title) || (contentItem && contentItem.title)}</Typography>
           </Breadcrumbs>
         </ModuleHeaderSubtitleContainer>
         <ModuleHeaderActionContainer>
         </ModuleHeaderActionContainer>
       </ModuleHeaderContainer>
       <ContentEditContainer>
-        {contentItem && JSON.stringify(contentItem)}
-        {isLoading && <div>Loading...</div>}
+        {
+          isLoading
+            ? <div>Loading...</div>
+            : <Card>
+              <CardContent>
+                <Grid container spacing={3}>
+                  <Grid xs={12} sm={6} item>
+                    <TextField
+                      disabled={props.readonly}
+                      label="Title"
+                      name="title"
+                      value={getValue("title")}
+                      placeholder="Enter title"
+                      variant="outlined"
+                      onChange={(e) => updateValue("title", e.target?.value)}
+                      fullWidth
+                    ></TextField>
+                  </Grid>
+                  <Grid item xs={6}></Grid>
+                  <Grid xs={12} sm={12} item>
+                    <MDEditor
+                      aria-disabled={props.readonly}
+                      minHeight={600}
+                      preview={"live"}
+                      contentEditable={!props.readonly}
+                      value={getValue("body") + ''}
+                      onChange={(e) => {
+                        !props.readonly && updateValue("body", e)
+                      }}/>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+        }
+
       </ContentEditContainer>
     </>
   );
