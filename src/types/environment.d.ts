9bb580e37795d8c6da4bd3662706646fa5aabab3
UniqueUlysees <boriscool007@gmail.com>
2023-03-14 14:35:14 +0300

    Removed redundant env variables

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -4,8 +4,6 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
         CORE_API: string;
-        MSAL_CLIENT_ID: string;
-        MSAL_AUTHORITY: string;
         CORE_API_SWAGGER: string | undefined;
     }
   }
