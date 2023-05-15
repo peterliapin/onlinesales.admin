70c70ed03308d30fc8a81648a3bdba6339299a23
Lakmal Priyadarshana <Lakmal.Priyadarshana@waveaccess.global>
2023-05-15 14:34:55 +0530

    json array data as a popup

--- src/components/generic-components/edit-components/array-edit.tsx
+++ src/components/generic-components/edit-components/array-edit.tsx
@@ -1,7 +1,8 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { EditProps } from "@components/generic-components/edit-components/common";
 import {
-  Autocomplete,
+  Dialog,
+  DialogTitle,
   Table,
   TableBody,
   TableCell,
@@ -9,9 +10,14 @@ import {
   TableRow,
   TextField,
 } from "@mui/material";
+import { camelCaseToTitleCase } from "../common";
 
 type JsonArray = {
   [key: string]: any;
+};
+
+const isJsonArray = (item: any): item is JsonArray => {
+  return typeof item === "object" && item !== null;
 };
 
 export const ArrayEdit = ({
@@ -20,30 +26,79 @@ export const ArrayEdit = ({
   example,
   required,
   value,
-  onChangeValue,
-  disabled,
   error,
-}: EditProps<JsonArray>): ReactNode => {
-  if (!value) return;
+}: EditProps<any>): ReactNode => {
+  const [open, setOpen] = useState(false);
+
+  if (!value) return <TextField key={key} label={label} value={"N/A"} disabled={true} />;
+
+  if (!isJsonArray(value)) {
+    const arrayString = value.join(",");
+    return (
+      <TextField
+        key={key}
+        title={example}
+        type={"text"}
+        label={label}
+        disabled={true}
+        required={required}
+        error={!!error}
+        helperText={error}
+        value={arrayString}
+        variant={"outlined"}
+        fullWidth={true}
+      />
+    );
+  }
+
   const headers = Object.keys(value![0]);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          {headers.map((header) => (
-            <TableCell key={header}>{header}</TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {value!.map((record: any) => (
-          <TableRow key={record.domainName}>
-            {headers.map((header) => (
-              <TableCell key={header}>{record[header]}</TableCell>
+    <>
+      <TextField
+        sx={{ input: { cursor: "pointer" } }}
+        key={key}
+        title={example}
+        type={"text"}
+        label={label}
+        disabled={true}
+        required={required}
+        error={!!error}
+        helperText={error}
+        value={"Click here to view"}
+        onClick={handleOpen}
+        variant={"outlined"}
+        fullWidth={true}
+      />
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{label}</DialogTitle>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {headers.map((header) => (
+                <TableCell key={header}>{camelCaseToTitleCase(header)}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {value!.map((record: any) => (
+              <TableRow key={record.domainName}>
+                {headers.map((header) => (
+                  <TableCell key={header}>{record[header]}</TableCell>
+                ))}
+              </TableRow>
             ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+          </TableBody>
+        </Table>
+      </Dialog>
+    </>
   );
 };
